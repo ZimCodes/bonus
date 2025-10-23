504793346463ac12f01dac5179529fc224be6c7f
ZimCodes <23222943+ZimCodes@users.noreply.github.com>
2025-10-23 15:13:44 -0500

    Makes changes to ibd-http-extractor project - Updates dependencies - Removes structopt dependency - Increase version specifier for regex dependency - Adds claprs dependency to replace structopt - Removes `-i` option - `paths` argument now takes a directory or file path(s) - Updates package metadata

--- ibd-http-extractor/src/lib.rs
+++ ibd-http-extractor/src/lib.rs
@@ -5,14 +5,10 @@ mod file;
 mod opts;
 pub fn extract_ibd() ->Result<(),io::Error>{
     let opts = opts::Opt::new();
-    let save_loc = match opts.output{
-        Some(path) => path,
-        None => PathBuf::from("./ibd_urls")
-    };
-
-    let paths = match opts.dir_path{
-        Some(path) => file::FileHandler::get_ibd_files(path),
-        None => opts.paths
+    let save_loc = opts.output.unwrap_or_else(|| PathBuf::from("./ibd_urls"));
+    let paths = match opts.paths[0].as_path().is_dir() {
+        true => file::FileHandler::get_ibd_files(opts.paths[0].clone()),
+        false => opts.paths
     };
 
     multiple_ibd_files(paths,save_loc)?;
--- ibd-http-extractor/src/opts.rs
+++ ibd-http-extractor/src/opts.rs
@@ -1,26 +1,21 @@
+use clap::Parser;
 use std::path::PathBuf;
-use structopt::StructOpt;
-#[derive(Debug,StructOpt)]
-#[structopt()]
-pub struct Opt{
+#[derive(Debug, Parser)]
+#[command()]
+pub struct Opt {
     /// Save Location
     ///
     /// The save location for the extracted links found in the ibd file.
-    #[structopt(short)]
-    pub output:Option<PathBuf>,
-    /// ibd directory path
+    #[arg(short)]
+    pub output: Option<PathBuf>,
+    /// ibd file path(s) or directory path
     ///
-    /// The directory where the ibd files are located.
-    #[structopt(long = "input",short = "i")]
-    pub dir_path:Option<PathBuf>,
-    /// ibd file path(s)
-    ///
-    /// The file path(s) of the .ibd file(s)
-    #[structopt(name = "Path(s)",required_unless("dir-path"))]
-    pub paths:Vec<PathBuf>,
+    /// The file path(s) or a directory path of the .ibd file(s)
+    #[arg(value_name = "Path(s)")]
+    pub paths: Vec<PathBuf>,
 }
-impl Opt{
-    pub fn new() ->Opt{
-        Opt::from_args()
+impl Opt {
+    pub fn new() -> Opt {
+        Opt::parse()
     }
 }
